00824e729edea80cff12edcca66daf50e10be392
Mohamed Samy <mohamedsabaawy@Hotmail.com>
2021-05-27 21:50:24 +0200

    Update ClientController.php

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 class ClientController extends Controller
 {
 //     ClientRequest $request
-    public function register(ClientRequest $request)
+    public function register(Request $request)
     {
 //         return 'sdfvbsdfb';
         $client = Client::create([
